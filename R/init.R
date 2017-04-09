11341b67f4106404f4261356c14618800456eeff
Pete Mohanty <pete.mohanty@gmail.com>
2017-04-08 17:19:09 -0700

    Update init.R

--- R/init.R
+++ R/init.R
@@ -1,6 +1,6 @@
 .pkgenv <- new.env(parent = emptyenv())
 # this code is adapted from Dirk Eddelbuettel's https://github.com/eddelbuettel/anytime/blob/ef8b1e52b80a99e96f46232dfe29180686327887/R/init.R#L49-L52
-# in light of this discussion: http://stackoverflow.com/questions/43247649/rcpparmadillo-bigmemory-crashes-windows-rstudio-but-no-other-gui-os-type
+# in light of this helpful discussion: http://stackoverflow.com/questions/43247649/rcpparmadillo-bigmemory-crashes-windows-rstudio-but-no-other-gui-os-type
 .onLoad <- function(libname, pkgname){
   
   packageStartupMessage("bigKRLS is authored by Pete Mohanty and Robert Shaffer.\n\nCheck out vignette(\"bigKRLS_basics\") for a brief explanation of the statistics, references, and syntax.\n\nFor the newest version, check out github.com/rdrr1990/bigKRLS/")
