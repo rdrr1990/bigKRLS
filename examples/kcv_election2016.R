e528e1b623b05a4ed4fe42d10cab6b6640774729
Pete Mohanty <pete.mohanty@gmail.com>
2018-03-10 11:37:56 -0800

    switching to cv

--- examples/kcv_election2016.R
+++ examples/kcv_election2016.R
@@ -6,28 +6,34 @@ replication_data <- read.table("https://raw.githubusercontent.com/rdrr1990/bigKR
 y <- as.matrix(replication_data$GOPdelta)
 X <- as.matrix(replication_data)[, 3:ncol(replication_data)]
 
-kcv <- function(startseed, Nreps){
+summaries <- list()
+
+cv <- function(startseed, Nreps){
   
   seeds <- startseed:(startseed + Nreps - 1)
   
   for(i in 1:Nreps){
     
-    overview <- summary(crossvalidate.bigKRLS(y, X, Kfolds = 5, 
-                                              Neig = 40, # Neig assumes bigKRLS 2.1.0 or newer 
-                                              seed = seeds[i]))[["overview"]]
-    
+    out <- crossvalidate.bigKRLS(y, X, ptesting = 20, 
+                                 Neig = 50, # Neig assumes bigKRLS 2.1.0 or newer 
+                                 seed = seeds[i])
+    summaries[[i]] <- summary(out)
+    overview <- summaries[[i]][["overview"]]
     
     overview <- cbind(overview, seeds[i])
-    
+
     if(i == 1){
-      overviews <- overview
+      overviews <- overview[-5,]
       colnames(overviews)[ncol(overviews)] <- "seed"
     }else{
-      overviews <- rbind(overviews, overview)
+      overviews <- rbind(overviews, overview[-5,])
     }
+    
     cat("\n\nfinished", i, "\n\n")
     write.csv(overviews, 
-         file = paste0("kcv_seeds_", startseed, "_to_", startseed + Nreps - 1, ".csv")) 
+         file = paste0("cv_seeds_", startseed, "_to_", startseed + Nreps - 1, ".csv")) 
+    save(summaries, 
+        file = paste0("cv_seeds_summaries_", startseed, "_to_", startseed + Nreps - 1, ".RData")) 
   }
 }
 
